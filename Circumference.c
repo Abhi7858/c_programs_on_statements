2232f60330806642dbbf0fa101948e07094f3e22
Abhi7858 <87105368+Abhi7858@users.noreply.github.com>
2021-07-24 15:23:16 +0530

    Calculate circumference

---
+++ Circumference.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ float radius;
+    float c;
+    printf("please enter a radius:");   
+    scanf("%f",&radius);
+    c=2*3.14*radius;
+    printf("%0.2f",c);
+    
+}
